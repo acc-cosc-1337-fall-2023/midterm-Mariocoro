0b78e11f9608aa0b783ddcf5726d4ebb9d97d2bf
Mariocoro <135136166+Mariocoro@users.noreply.github.com>
2023-10-12 00:33:08 +0000

    question_4

--- src/question_4/main.cpp
+++ src/question_4/main.cpp
@@ -1,6 +1,15 @@
+#include <iostream>
 
-int main()
-{
-
+using namespace std;
+string transcribe_dna_into_rna_ma(string & str){
+    
+    str[2]=str[8]=str[9]=str[13]=str[17]=str[21]=str[22]='U';
+    
+    return str;
+    
+}
+int main(){
+    string str = "GATGGAACTTGACTACGTAAATT";
+    cout<<transcribe_dna_into_rna_ma(str); 
     return 0;
 }
